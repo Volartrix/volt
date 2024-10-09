8636f02d1bd2052939570f2a227e32514bb44e55
RaphtikAtGHG <raphael.wegmann81@gmail.com>
2024-10-09 22:14:36 +0200

    VMM ig but no fucking idea if it works

---
+++ kernel/src/include/mm/vmm.h
@@ -0,0 +1,67 @@
+#ifndef __VMM_H__
+#define __VMM_H__
+
+#include <stdint.h>
+#include <stddef.h>
+#include <mm/pmm.h>
+#include <string.h>
+
+extern struct limine_memmap_response* memmap;
+extern uint64_t  kernel_phys_base;
+extern uint64_t  kernel_virt_base;
+
+extern uint64_t p_kernel_start[];
+extern uint64_t p_writeallowed_start[];
+extern uint64_t p_kernel_end[];
+
+extern uint64_t *pml4_global;
+
+// thx UnmappedStack
+#define KERNEL_PFLAG_PRESENT 0b1
+#define KERNEL_PFLAG_WRITE   0b10
+#define KERNEL_PFLAG_USER    0b100
+#define KERNEL_PFLAG_PXD     0b10000000000000000000000000000000000000000000000000000000000000 // a bit long lmao
+
+#define PAGE_SIZE 4096
+#define KERNEL_STACK_PAGES 2LL
+#define KERNEL_STACK_PTR 0xFFFFFFFFFFFFF000LL
+#define KERNEL_STACK_ADDR KERNEL_STACK_PTR-(KERNEL_STACK_PAGES*PAGE_SIZE)
+
+#define USER_STACK_PAGES 2LL
+#define USER_STACK_ADDR (USER_STACK_PTR - USER_STACK_PAGES*PAGE_SIZE)
+#define USER_STACK_PTR 0x700000000000LL
+
+#define PAGE_ALIGN_DOWN(addr) ((addr / 4096) * 4096) // works cos of integer division
+#define PAGE_ALIGN_UP(x) ((((x) + 4095) / 4096) * 4096)
+
+#define KERNEL_SWITCH_PAGE_TREE(TREE_ADDRESS) \
+    __asm__ volatile (\
+       "movq %0, %%cr3"\
+       :\
+       :  "r" (TREE_ADDRESS)\
+    )
+
+#define KERNEL_SWITCH_STACK() \
+    __asm__ volatile (\
+       "movq %0, %%rsp\n"\
+       "movq $0, %%rbp\n"\
+       "push $0"\
+       :\
+       :  "r" (KERNEL_STACK_PTR)\
+    )
+
+void init_vmm();
+uint64_t* init_paging_task();
+uint64_t virt_to_phys(uint64_t pml4[], uint64_t virt_addr);
+uint64_t phys_to_virt(uint64_t phys_addr);
+void write_vmem(uint64_t *pml4_addr, uint64_t virt_addr, char *data, size_t len);
+void push_vmem(uint64_t *pml4_addr, uint64_t rsp, char *data, size_t len);
+void map_pages(uint64_t pml4_addr[], uint64_t virt_addr, uint64_t phys_addr, uint64_t num_pages, uint64_t flags);
+void alloc_pages(uint64_t pml4_addr[], uint64_t virt_addr, uint64_t num_pages, uint64_t flags);
+
+
+void map_sections(uint64_t pml4[]);
+void map_kernel(uint64_t pml4[]);
+void map_all(uint64_t pml4[]);
+
+#endif // __VMM_H__
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -4,10 +4,15 @@
 #include <hal/x64/idt.h>
 #include <limine.h>
 #include <mm/pmm.h>
+#include <mm/vmm.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+
+extern uint64_t p_kernel_start[];
+extern uint64_t p_writeallowed_start[];
+extern uint64_t p_kernel_end[];
 
 struct flanterm_context* ft_ctx;
 
@@ -28,6 +33,10 @@ __attribute__((used,
     hhdm_request = {.id = LIMINE_HHDM_REQUEST, .revision = 0};
 
 __attribute__((used,
+               section(".requests"))) static volatile struct limine_kernel_address_request
+    kernel_addr_request = {.id = LIMINE_KERNEL_ADDRESS_REQUEST, .revision = 0};
+
+__attribute__((used,
                section(".requests_start_"
                        "marker"))) static volatile LIMINE_REQUESTS_START_MARKER;
 
@@ -37,7 +46,12 @@ __attribute__((
         ".requests_end_marker"))) static volatile LIMINE_REQUESTS_END_MARKER;
 
 uint64_t                       hhdm_offset = 0;
+uint64_t                       hddm_size = 0;
 struct limine_memmap_response* memmap      = NULL;
+struct limine_kernel_address_response* kernel_addr = NULL;
+uint64_t  kernel_phys_base = 0;
+uint64_t  kernel_virt_base = 0;
+
 
 static void hcf(void) {
     for (;;) {
@@ -45,6 +59,7 @@ static void hcf(void) {
     }
 }
 
+uint64_t *pml4_global = NULL;
 
 void kmain(void) {
     if (LIMINE_BASE_REVISION_SUPPORTED == false) {
@@ -69,7 +84,9 @@ void kmain(void) {
 
     hhdm_offset = hhdm_request.response->offset;
     memmap      = memmap_request.response;
-
+    kernel_addr = kernel_addr_request.response;
+    kernel_phys_base = kernel_addr->physical_base;
+    kernel_virt_base = kernel_addr->virtual_base;
 
     ft_ctx->cursor_enabled = false;
     ft_ctx->tab_size       = 2;
@@ -78,13 +95,18 @@ void kmain(void) {
     idt_init();
 
     pmm_init();
+    init_vmm();
+
+    pml4_global = init_paging_task();
 
     uint64_t free_memory = pmm_get_free();
 
-    printf("Free memory: %d bytes\n", free_memory);
+    printf("Free memory: %dMB bytes\n\n", free_memory / 1024 / 1024);
 
+    printf("Kernel Start: %.16llX\n", p_kernel_start);
+    printf("Write Allowed Start: %.16llX\n", p_writeallowed_start);
+    printf("Kernel End: %.16llX\n\n", p_kernel_end);
 
-    asm("int $0x03");
 
     hcf();
 }
